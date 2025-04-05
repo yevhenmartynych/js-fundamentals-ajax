748c331e52bc577a9c220e5420c0d95c74806969
Yevhen Martynych <yevhenmartynych@gmail.com>
2025-04-05 13:16:46 +0300

    implement fetch and display user data

--- task1.js
+++ task1.js
@@ -1,7 +1,18 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди node tests/task1.test.js
+
+const usersList = document.querySelector('.usersList')
+const usersApi = 'https://jsonplaceholder.typicode.com/users'
+
+fetch(usersApi)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((user) => {
+      usersList.innerHTML += `<li>${user.username}</li>`
+    })
+  })
--- task2.js
+++ task2.js
@@ -5,3 +5,18 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userNameInput = document.querySelector('#userNameInput')
+const getUserButton = document.querySelector('#getUserButton')
+const userCity = document.querySelector('#userCity')
+const userApi = 'https://jsonplaceholder.typicode.com/users'
+
+getUserButton.addEventListener('click', async () => {
+  const userInput = userNameInput.value
+
+  const response = await fetch(userApi)
+  const user = await response.json()
+
+  const result = user.find((item) => item.name == userInput)
+  userCity.innerHTML = `${result.address.city}`
+})
